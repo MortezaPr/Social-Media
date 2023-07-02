11bb8a47602432f09c124fcc86d3ebe824cb082d
Morteza Pourramzan <pourramzanmorteza@gmail.com>
2023-07-02 12:22:34 +0330

    added all the necessary schemas

--- share-space_backend/schemas/index.ts
+++ share-space_backend/schemas/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from './user.js'
+import pin from './pin.js'
+import comment from './comment.js'
+import postedBy from './postedBy.js'
+import save from './save.js'
+
+export const schemaTypes = [user, pin, comment, postedBy, save]
---
+++ share-space_backend/schemas/pin.js
@@ -0,0 +1,65 @@
+export default {
+  name: 'pin',
+  title: 'Pin',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'string',
+    },
+    {
+      name: 'destination',
+      title: 'Destination',
+      type: 'string',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotshot: true,
+      },
+    },
+    {
+      name: 'userID',
+      title: 'UserID',
+      type: 'string',
+    },
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedBy',
+    },
+    {
+      name: 'save',
+      title: 'Save',
+      type: 'array',
+      of: [
+        {
+          type: 'save',
+        },
+      ],
+    },
+    {
+      name: 'comments',
+      title: 'Comments',
+      type: 'array',
+      of: [
+        {
+          type: 'comment',
+        },
+      ],
+    },
+  ],
+}
