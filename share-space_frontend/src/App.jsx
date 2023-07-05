4856f445912ae7216b6ca7d6d7dcf84f20372581
Morteza Pourramzan <morteza9427@gmail.com>
2023-07-06 01:47:06 +0330

    added login

--- share-space_frontend/src/App.jsx
+++ share-space_frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
 import "./App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import Login from "./components/Login";
+import Home from "./container/Home";
+
+const clientId =
+  "182109303730-btm0n87hhviiss0mij1g4krmrjqih221.apps.googleusercontent.com";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
+  );
 }
 
 export default App;
---
+++ share-space_frontend/src/components/Login.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
+const Login = () => {
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="p-5">
+          <img src={logo} width="130px" alt="logo" />
+        </div>
+        <div className="shadow-2xl">
+          <GoogleLogin clie onSuccess={() => console.log("yess")} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
