7e0c2f4a5d69672f50e712ebb7bdfe49a7b8028d
Morteza Pourramzan <morteza9427@gmail.com>
2023-07-12 18:51:21 +0330

    refactor code, fetch user

--- share-space_frontend/src/container/Home.jsx
+++ share-space_frontend/src/container/Home.jsx
@@ -7,15 +7,13 @@ import { Sidebar, UserProfile } from "../components";
 import { client } from "../client";
 import Pins from "./Pins";
 import { userQuery } from "../utils/data";
+import { fetchUser } from "../utils/fetchUser";
 import logo from "../assets/logo.png";
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
   const scrollRef = useRef(null);
-  const userInfo =
-    localStorage.getItem("user") !== "undefined"
-      ? JSON.parse(localStorage.getItem("user"))
-      : localStorage.clear();
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?._id);
