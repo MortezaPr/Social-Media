93d1d022f5209836e799b189da98410cdc324fb2
Morteza Pourramzan <morteza9427@gmail.com>
2023-07-14 02:42:27 +0330

    now users have to login first

--- share-me_frontend/src/App.jsx
+++ share-me_frontend/src/App.jsx
@@ -3,8 +3,17 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Login from "./components/Login";
 import Home from "./container/Home";
+import { useEffect } from "react";
+import { fetchUser } from "./utils/fetchUser";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+    if (!user) navigate("/login");
+  }, []);
+
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}>
       <Routes>
